0bd39696b085bc362585f2682fefe19d64c2f108
johnelmer1980 <john.elmer@lexisnexisrisk.com>
2025-08-19 12:48:40 +0100

    added node.js server

--- pages/api/index2.js
+++ pages/api/index2.js
@@ -1,9 +1,8 @@
 export default function handler(req, res) {
   if (req.method === "POST") {
     const { org_id, hostname, policy } = req.body;
-    // Do whatever your PHP did here
-    res.json({ org_id, hostname, policy });
+    res.status(200).json({ org_id, hostname, policy });
   } else {
-    res.status(405).end(); // Method Not Allowed
+    res.status(405).end(); // Method not allowed
   }
 }
